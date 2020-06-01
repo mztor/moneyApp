191c2e85dc4cf1f312ac14428397a3cd39931a4a
mztor <stor@macarthur.nsw.edu.au>
2020-06-02 08:45:51 +1000

    Add files via upload

---
+++ salesTax.py
@@ -0,0 +1,14 @@
+def error_message():
+    print("Something went wrong!")
+
+
+def calculate_sales_tax(chosenState, price):
+    #this function looks up the sales tax file and calculates the sales tax
+    #based on the price
+    salePrice = 0
+    return salePrice
+
+
+#currently only USA, eventually, we will extend globally, eg:
+#Australia 10% GST
+
